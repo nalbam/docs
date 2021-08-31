b82ec8e9ed94b5469f6cf8b7cca4a1a31a74299b
nalbam <me@nalbam.com>
2021-08-31 21:21:40 +0900

    owner

--- dispatch.sh
+++ dispatch.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-curl \
+curl -sL \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/nalbam/argocd-env-demo/dispatches \
-  -d '{"event_type":"gitops"}'
-
-# curl -sL \
-#   -X POST \
-#   -H "Accept: application/vnd.github.v3+json" \
-#   https://api.github.com/repos/nalbam/argocd-env-demo/dispatches \
-#   -d '{"event_type":"gitops","client_payload":{"project":"nalbam-docs","version":"v0.1.1"}}'
+  -d '{"owner":"nalbam","repo":"argocd-env-demo","event_type":"gitops","client_payload":{"project":"nalbam-docs","version":"v0.1.1"}}'
