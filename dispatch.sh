a97cde712c4d97d78596de0e58469b76535bdfa3
nalbam <me@nalbam.com>
2021-08-31 21:34:14 +0900

    Authorization

--- dispatch.sh
+++ dispatch.sh
@@ -3,6 +3,6 @@
 curl -sL \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/nalbam/argocd-env-demo/dispatches \
-  -d '{"owner":"nalbam","repo":"argocd-env-demo","event_type":"gitops","client_payload":{"project":"nalbam-docs","version":"v0.1.1"}}'
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -d '{"owner":"nalbam","repo":"argocd-env-demo","event_type":"gitops","client_payload":{"project":"nalbam-docs","version":"v0.1.1"}}' \
+  https://api.github.com/repos/nalbam/argocd-env-demo/dispatches
